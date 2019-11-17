bf91747eb11b498a14fced634479c322c148fbfe
Jessica Trinh <jtrinh@cs.ucsb.edu>
2019-11-16 16:20:05 -0800

    change first link from Page 1 to Earthquakes

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -71,4 +71,12 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/a").exists())
                 .andExpect(xpath("/html/body/div/nav/a").string("lab07"));
     }
+
+    @Test
+    public void getHomePage_hasCorrectFirstLink() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/div//ul/li[2]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div//ul/li[2]/a").string("Earthquakes"));
+    }
 }
